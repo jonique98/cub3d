5ecdfbbf2330b41027d5aa983f2e375d7ea0c1ba
jonique98 <tnals9899@naver.com>
2024-02-11 05:56:38 +0900

    move restriction

--- man_src/move.c
+++ man_src/move.c
@@ -6,7 +6,7 @@
 /*   By: sumjo <sumjo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 05:07:43 by sumjo             #+#    #+#             */
-/*   Updated: 2024/02/11 05:43:33 by sumjo            ###   ########.fr       */
+/*   Updated: 2024/02/11 05:56:07 by sumjo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	move_forward(t_var *var)
 
 	x = (int)(var->vec->pos.x + var->vec->dir.x * var->frame->moveSpeed);
 	y = (int)(var->vec->pos.y);
-	if (var->map->map[x][y] == 0)
+	if (var->map->map[(int)(var->vec->pos.x + var->vec->dir.x)][y] == 0)
 		var->vec->pos.x += var->vec->dir.x * var->frame->moveSpeed;
 	x = (int)(var->vec->pos.x);
 	y = (int)(var->vec->pos.y + var->vec->dir.y * var->frame->moveSpeed);
-	if (var->map->map[x][y] == 0)
+	if (var->map->map[x][(int)(var->vec->pos.y + var->vec->dir.y)] == 0)
 		var->vec->pos.y += var->vec->dir.y * var->frame->moveSpeed;
 }
 
@@ -34,11 +34,11 @@ void	move_backward(t_var *var)
 
 	x = (int)(var->vec->pos.x - var->vec->dir.x * var->frame->moveSpeed);
 	y = (int)(var->vec->pos.y);
-	if (var->map->map[x][y] == 0)
+	if (var->map->map[(int)(var->vec->pos.x - var->vec->dir.x)][y] == 0)
 		var->vec->pos.x -= var->vec->dir.x * var->frame->moveSpeed;
 	x = (int)(var->vec->pos.x);
 	y = (int)(var->vec->pos.y - var->vec->dir.y * var->frame->moveSpeed);
-	if (var->map->map[x][y] == 0)
+	if (var->map->map[x][(int)(var->vec->pos.y - var->vec->dir.y)] == 0)
 		var->vec->pos.y -= var->vec->dir.y * var->frame->moveSpeed;
 }
 
@@ -47,14 +47,14 @@ void	move_left(t_var *var)
 	int	x;
 	int	y;
 
-	x = (int)(var->vec->pos.x - var->vec->dir.y * var->frame->moveSpeed);
-	y = (int)var->vec->pos.y;
-	if (var->map->map[x][y] == 0)
-		var->vec->pos.x -= var->vec->dir.y * var->frame->moveSpeed;
-	x = (int)var->vec->pos.x;
-	y = (int)(var->vec->pos.y + var->vec->dir.x * var->frame->moveSpeed);
-	if (var->map->map[x][y] == 0)
-		var->vec->pos.y += var->vec->dir.x * var->frame->moveSpeed;
+	x = (int)(var->vec->pos.x - var->vec->plane.x * var->frame->moveSpeed);
+	y = (int)(var->vec->pos.y);
+	if (var->map->map[(int)(var->vec->pos.x - var->vec->plane.x)][y] == 0)
+		var->vec->pos.x -= var->vec->plane.x * var->frame->moveSpeed;
+	x = (int)(var->vec->pos.x);
+	y = (int)(var->vec->pos.y - var->vec->plane.y * var->frame->moveSpeed);
+	if (var->map->map[x][(int)(var->vec->pos.y - var->vec->plane.y)] == 0)
+		var->vec->pos.y -= var->vec->plane.y * var->frame->moveSpeed;
 }
 
 void	move_right(t_var *var)
@@ -62,12 +62,13 @@ void	move_right(t_var *var)
 	int	x;
 	int	y;
 
-	x = (int)(var->vec->pos.x + var->vec->dir.y * var->frame->moveSpeed);
-	y = (int)var->vec->pos.y;
-	if (var->map->map[x][y] == 0)
-		var->vec->pos.x += var->vec->dir.y * var->frame->moveSpeed;
-	x = (int)var->vec->pos.x;
-	y = (int)(var->vec->pos.y - var->vec->dir.x * var->frame->moveSpeed);
-	if (var->map->map[x][y] == 0)
-		var->vec->pos.y -= var->vec->dir.x * var->frame->moveSpeed;
+	x = (int)(var->vec->pos.x + var->vec->plane.x * var->frame->moveSpeed);
+	y = (int)(var->vec->pos.y);
+	if (var->map->map[(int)(var->vec->pos.x + var->vec->plane.x)][y] == 0)
+		var->vec->pos.x += var->vec->plane.x * var->frame->moveSpeed;
+	x = (int)(var->vec->pos.x);
+	y = (int)(var->vec->pos.y + var->vec->plane.y * var->frame->moveSpeed);
+	if (var->map->map[x][(int)(var->vec->pos.y + var->vec->plane.y)] == 0)
+		var->vec->pos.y += var->vec->plane.y * var->frame->moveSpeed;
 }
+
